8bdc8c15cd1204801c8818e7692ff484d0312c5f
sandy1999 <sandyrockstar19@gmail.com>
2018-09-18 20:03:53 +0530

    added: pokemon details

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,24 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'pokemon.dart';
-import 'dart:convert';
-void main() => runApp(new PokeApp());
+import 'package:pokemonapp/pokemon.dart';
+import 'package:pokemonapp/pokemondetail.dart';
 
-class PokeApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Poke App',
-      theme: new ThemeData(
-        primarySwatch: Colors.cyan,
-      ),
-      home: new MyHomePage(title: 'Poke App'),
+void main() => runApp(MaterialApp(
+      title: "Poke App",
+      home: HomePage(),
       debugShowCheckedModeBanner: false,
-    );
+    ));
+
+class HomePage extends StatefulWidget {
+  @override
+  HomePageState createState() {
+    return new HomePageState();
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
+class HomePageState extends State<HomePage> {
   var url =
       "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
 
@@ -47,52 +38,67 @@ class _MyHomePageState extends State<MyHomePage> {
     print(pokeHub.toJson());
     setState(() {});
   }
+
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title, textAlign: TextAlign.center,),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Poke App"),
+        backgroundColor: Colors.cyan,
       ),
-      body: 
-      pokeHub == null ? Center(
-        child: CircularProgressIndicator(),
-        ):
-        GridView.count(
-          crossAxisCount: 2,
-          children: pokeHub.pokemon.map((poke)=>Padding(
-            padding: const EdgeInsets.all(2.0),
-            child: Card(
-              elevation: 3.0,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Container(
-                    height: 100.0,
-                    width: 100.0,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: NetworkImage(poke.img)
-                      )
-                    ),
-                  ),
-                  Text(poke.name,
-                   style: TextStyle(
-                     fontSize: 20.0,
-                     fontWeight: FontWeight.bold
-                   ),)
-                ],
-              ),
+      body: pokeHub == null
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : GridView.count(
+              crossAxisCount: 2,
+              children: pokeHub.pokemon
+                  .map((poke) => Padding(
+                        padding: const EdgeInsets.all(2.0),
+                        child: InkWell(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => PokeDetail(
+                                          pokemon: poke,
+                                        )));
+                          },
+                          child: Hero(
+                            tag: poke.img,
+                            child: Card(
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: <Widget>[
+                                  Container(
+                                    height: 100.0,
+                                    width: 100.0,
+                                    decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                            image: NetworkImage(poke.img))),
+                                  ),
+                                  Text(
+                                    poke.name,
+                                    style: TextStyle(
+                                      fontSize: 20.0,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ))
+                  .toList(),
             ),
-          )).toList(),
-        ),
-      drawer: new Drawer(),
-      floatingActionButton: new FloatingActionButton(
+      drawer: Drawer(),
+      floatingActionButton: FloatingActionButton(
         onPressed: () {},
-        tooltip: 'Refresh',
-        child: new Icon(Icons.refresh),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        backgroundColor: Colors.cyan,
+        child: Icon(Icons.refresh),
+      ),
     );
   }
 }
